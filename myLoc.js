35d5f3f09007eeea30b2a6af2f4720b383d6ad0a
yarivdo <yarivdo@gmail.com>
2014-12-10 18:28:45 +1300

    First Web for Geolocation

---
+++ myLoc.js
@@ -0,0 +1,18 @@
+﻿window.onload = getMyLocation;
+
+function getMyLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayLocation);
+    }
+    else {
+        alert("Oops, no geolocation support");
+    }
+}
+
+function displayLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var div = document.getElementById("location");
+    div.innerHTML = "You are at latitude: " + latitude + " , Longitude: " + longitude;
+}
